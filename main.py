2f08625829c6889c16fdf4ab3a9d74d4dcf3ee67
delovoyhomie <slavik-yakimenko@yandex.ru>
2023-05-19 08:25:03 +0300

    edited and created commands

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher.filters import Command
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.dispatcher.filters import Command, ChatTypeFilter
 import sqlite3, json
-from token import TOKEN
+from config import TOKEN
 from api import proposal
 
 bot = Bot(token=TOKEN)
@@ -20,28 +20,44 @@ class States(StatesGroup):
     AddDAOAddress = State()
 
 ########################################################
+# Приветственная команда для группы
 @dp.message_handler(commands=['start'], state='*', chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer("Привет! это публичка... ")
+
+# Приветственная команда для личных сообщений
+@dp.message_handler(commands=['start'], state='*',  chat_type=types.ChatType.PRIVATE)
+async def cmd_start(message: types.Message, state: FSMContext):
+    await message.answer("Привет! Инструкция: ")
+
+# Установка адреса DAO
+@dp.message_handler(commands=['set'], state='*', chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
+async def cmd_set(message: types.Message, state: FSMContext):
+    # Проверка на администратора или создателя группы
     chat_admins = await bot.get_chat_administrators(message.chat.id)
     for user in chat_admins:
         if (user['status'] == 'administrator' and user['user']['id'] == message.from_user.id) or (user['status'] == 'creator' and user['user']['id'] == message.from_user.id):
             await message.answer("Пожалуйста, введите адрес DAO ответным сообщением")
             await States.AddDAOAddress.set()
 
-
+# Связанный состояние для установки вдреса DAO 
 @dp.message_handler(state = States.AddDAOAddress, chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
 async def handle_message(message: types.Message, state: FSMContext):
     if message.text:
+        # Проверка на администратора или создателя группы
         chat_admins = await bot.get_chat_administrators(message.chat.id)
         for user in chat_admins:
             if (user['status'] == 'administrator' and user['user']['id'] == message.from_user.id) or (user['status'] == 'creator' and user['user']['id'] == message.from_user.id):
-                # Вместо простого ответа вы можете сохранить адрес в базе данных или где-то еще
                 dao_address = message.text
                 group_id = message.chat.id
                 await message.answer(f"Вы ввели следующий адрес: {dao_address}")
                 await state.finish()
-                cursor.execute(f"INSERT INTO DAOs (dao_address, group_id) VALUES ('{dao_address}', '{group_id}')"), conn.commit()  
+                cursor.execute(f"INSERT INTO DAOs (dao_address, group_id) VALUES ('{dao_address}', '{group_id}')"), conn.commit() # Добавление в базу данных адрес DAO
         
+# Редактирование адреса DAO
+# @dp.message_handler(commands=['reset'], state='*', chat_type=[types.ChatType.GROUP, types.ChatType.SUPERGROUP])
+# async def cmd_reset(message: types.Message, state: FSMContext):
+
 
 ########################################################
 if __name__ == '__main__':
