59cf52ee43ed0086756d069f686f8ba8004cee60
delovoyhomie <slavik-yakimenko@yandex.ru>
2023-05-19 09:31:50 +0300

    added new functions

--- api.py
+++ api.py
@@ -1,9 +1,59 @@
 import requests, json
 
-def proposal(address_dao):
+def daos(daoAddress):
+
+    # Запрос на json с DAOs
+    url = f'https://dev-ton-vote-cache.herokuapp.com/daos'
+
+    try:
+        response = requests.get(url)
+        data = response.json()  # Получение JSON-данных из ответа
+        
+        return data
+
+        # for i in range(len(data)):
+        #     address = data[i]['daoAddress'] 
+        #     if address == daoAddress:
+        #         print(data[i])
+        #         return data[i]
+            
+        # return 'Not Found'
+
+    except Exception as e:
+        print('Произошла ошибка при выполнении запроса (/daos):', e)
+
+# print(daos('EQDi7_28cJItXu5t5evsnEKNtUYv_1aQve21T4bzFxbxJ8HF'))
+
+
+def daoAddressInfo(daoAddress):
+    # Запрос на json с предложениями по DAO
+    url = f'https://dev-ton-vote-cache.herokuapp.com/dao/{daoAddress}'
+
+    try:
+        response = requests.get(url)
+        data = response.json()  # Получение JSON-данных из ответа
+
+        about = json.loads(data['daoMetadata']['about'])['en']
+        avatar = data['daoMetadata']['avatar']
+        website = data['daoMetadata']['github'] 
+        name = json.loads(data['daoMetadata']['name'])['en']
+        telegram = data['daoMetadata']['telegram']
+        website = data['daoMetadata']['website']
+        
+        print(about, avatar, website, name, telegram, website, sep = '\n')
+
+        return data
+
+    except Exception as e:
+        print('Произошла ошибка при выполнении запроса (/dao/daoAddress):', e)
+
+print(daoAddressInfo('EQDi7_28cJItXu5t5evsnEKNtUYv_1aQve21T4bzFxbxJ8HF'))
+
+
+def proposal(daoAddress):
 
     # Запрос на json с предложениями по DAO
-    url = f'https://dev-ton-vote-cache.herokuapp.com/proposal/{address_dao}'
+    url = f'https://dev-ton-vote-cache.herokuapp.com/proposal/{daoAddress}'
 
     try:
         response = requests.get(url)
@@ -13,7 +63,7 @@ def proposal(address_dao):
         # Обработка полученных данных
         # ...daoAddress
 
-    except:
-        print('Произошла ошибка при выполнении запроса (/daos):')
+    except Exception as e:
+        print('Произошла ошибка при выполнении запроса (/proposal/address_dao):', e)
     
 # print(proposal('EQCc8vSY5grdLTeTjqUnnCHDQ0mkf906cWf1ap_g0LeRShSg'))
